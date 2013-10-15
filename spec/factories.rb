03cef177aaf3faa7cb12924da3d4b0df3beaaa29
Adrian Freedman <adrianrfreedman@gmail.com>
2013-10-15 14:29:06 +1100

    Finish user sign up

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Peggy Sue"
+    email    "peggys@itf.com"
+    password "foofar"
+    password_confirmation "foofar"
+  end
+end
+
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,10 +4,59 @@ describe "User pages" do
 
   subject { page }
 
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
+
   describe "signup page" do
     before { visit signup_path }
 
     it { should have_content('Sign up') }
     it { should have_title(full_title 'Sign up') }
+  end
+
+  describe "signup" do
+
+    before { visit signup_path }
+    
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Jon P Pants"
+        fill_in "Email",        with: "jonppants@itf.com"
+        fill_in "Password",     with: "secret"
+        fill_in "Confirmation", with: "secret"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'jonppants@itf.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+    end
   end
 end
